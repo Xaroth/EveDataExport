55b03b9a46ea4f0c42c6350896d32043b6822fa7
Xaroth Brook <devnull@xaroth.nl>
2013-02-09 17:17:54 +0100

    -Change: re-worked eve.eve sub-app

--- dataexport/eve/eve/models.py
+++ dataexport/eve/eve/models.py
@@ -1,29 +1,35 @@
 from django.db import models
 
-class Evegraphics(models.Model):
-    graphicid = models.IntegerField(primary_key=True, db_column='graphicID') # Field name made lowercase.
-    graphicfile = models.CharField(max_length=1500, db_column='graphicFile') # Field name made lowercase.
-    description = models.TextField()
-    obsolete = models.IntegerField()
-    graphictype = models.CharField(max_length=300, db_column='graphicType', blank=True) # Field name made lowercase.
-    collidable = models.IntegerField(null=True, blank=True)
-    explosionid = models.IntegerField(null=True, db_column='explosionID', blank=True) # Field name made lowercase.
-    directoryid = models.IntegerField(null=True, db_column='directoryID', blank=True) # Field name made lowercase.
-    graphicname = models.CharField(max_length=192, db_column='graphicName') # Field name made lowercase.
-    class Meta:
-        db_table = u'eveGraphics'
+class Graphic(models.Model):
+    id                  = models.IntegerField(primary_key=True, db_column='graphicID') # Field name made lowercase.
+    filename            = models.CharField(max_length=1500, db_column='graphicFile') # Field name made lowercase.
+    description         = models.TextField()
+    obsolete            = models.IntegerField()
+    graphictype         = models.CharField(max_length=300, db_column='graphicType', blank=True) # Field name made lowercase.
+    collidable          = models.IntegerField(null=True, blank=True)
+    explosionid         = models.IntegerField(null=True, db_column='explosionID', blank=True) # Field name made lowercase.
+    directoryid         = models.IntegerField(null=True, db_column='directoryID', blank=True) # Field name made lowercase.
+    graphicname         = models.CharField(max_length=192, db_column='graphicName') # Field name made lowercase.
 
-class Eveicons(models.Model):
-    iconid = models.IntegerField(primary_key=True, db_column='iconID') # Field name made lowercase.
-    iconfile = models.CharField(max_length=1500, db_column='iconFile') # Field name made lowercase.
-    description = models.TextField()
     class Meta:
-        db_table = u'eveIcons'
+        db_table        = u'eveGraphics'
+        ordering        = ('id',)
 
-class Eveunits(models.Model):
-    unitid = models.IntegerField(primary_key=True, db_column='unitID') # Field name made lowercase.
-    unitname = models.CharField(max_length=300, db_column='unitName', blank=True) # Field name made lowercase.
-    displayname = models.CharField(max_length=150, db_column='displayName', blank=True) # Field name made lowercase.
-    description = models.CharField(max_length=3000, blank=True)
+class Icon(models.Model):
+    id                  = models.IntegerField(primary_key=True, db_column='iconID') # Field name made lowercase.
+    filename            = models.CharField(max_length=1500, db_column='iconFile') # Field name made lowercase.
+    description         = models.TextField()
+
     class Meta:
-        db_table = u'eveUnits'
+        db_table        = u'eveIcons'
+        ordering        = ('id',)
+
+class Unit(models.Model):
+    id                  = models.IntegerField(primary_key=True, db_column='unitID') # Field name made lowercase.
+    name                = models.CharField(max_length=300, db_column='unitName', blank=True) # Field name made lowercase.
+    displayname         = models.CharField(max_length=150, db_column='displayName', blank=True) # Field name made lowercase.
+    description         = models.CharField(max_length=3000, blank=True)
+
+    class Meta:
+        db_table        = u'eveUnits'
+        ordering        = ('id',)
