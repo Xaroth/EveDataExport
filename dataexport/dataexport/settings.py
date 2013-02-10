7da1e93e2b8e37d36856c1b4392f9ee762613669
Xaroth Brook <devnull@xaroth.nl>
2013-02-10 15:39:02 +0100

    -Add: logging specs to settings

--- dataexport/dataexport/settings.py
+++ dataexport/dataexport/settings.py
@@ -143,15 +143,35 @@ INSTALLED_APPS += [
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
+from logging.handlers import SysLogHandler 
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(name)s %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
     'filters': {
         'require_debug_false': {
             '()': 'django.utils.log.RequireDebugFalse'
         }
     },
     'handlers': {
+        'syslog': {
+            'address': '/dev/log',
+            'class': 'logging.handlers.SysLogHandler',
+            'formatter': 'verbose',
+            'facility': SysLogHandler.LOG_LOCAL2,
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
         'mail_admins': {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
@@ -166,3 +186,5 @@ LOGGING = {
         },
     }
 }
+app_loggers = dict([(x, {'handlers': ['console',], 'level': 'DEBUG', 'propagate': True}) for x in INSTALLED_APPS if not x.startswith('django')])
+LOGGING['loggers'].update(app_loggers)
